ac74d195e3417f035fe5746c22cdb2dd3800481a
Tamzid Ahmed Apurbo <209776781+A-P-U-R-B-O@users.noreply.github.com>
2025-07-12 16:50:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def reset():
     return jsonify({"reply": "Memory wiped 🧽 — let's start fresh!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=81)
+    app.run(host="0.0.0.0", port=8080)
